3cff87c4578fe86d23eaddcd5528ffa2fe99be91
Jonathan <jonathan962911@gmail.com>
2020-06-08 00:26:37 -0500

    Agregnado usuarioa lista de usuarios conectados

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,12 +1,18 @@
 import { Socket, Server } from 'socket.io';
+import { ListaUsuarios } from 'src/classes/listaUsuarios.class';
+import { Usuario } from '../classes/usuario.model';
 
+
+export const usuariosConectados = new ListaUsuarios();
 
 export const desconectar = (cliente: Socket) => {
+    usuariosConectados.deleteUser(cliente.id);
 };
 
 
 export const conectar = (cliente: Socket) => {
-
+    const usuario: Usuario = new Usuario(cliente.id)
+    usuariosConectados.agregarUsuario(usuario);
 };
 
 export const mensaje = (cliente: Socket, payload: any, io: Server) => {
@@ -14,5 +20,6 @@ export const mensaje = (cliente: Socket, payload: any, io: Server) => {
 };
 
 export const configurarUsuario = (cliente: Socket, payload: any, io: Server) => {
-    return {success: true, message: `Usuario ${payload.nombre} configurado correctamente`}
+    usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
+    return { success: true, message: `Usuario ${payload.nombre} configurado correctamente` };
 }
